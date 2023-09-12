23ee64021e4f975cae3630ea2cd00e60712ce1c1
jonbaetz-qz <59665934+jonbaetz-qz@users.noreply.github.com>
2023-09-12 15:09:27 -0700

    Initial commit

---
+++ apps/game/next.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  reactStrictMode: true,
+  transpilePackages: ['dataset'],
+  basePath: '/game',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.staticflickr.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.quizlet.com',
+        port: '',
+      },
+    ],
+  },
+};
